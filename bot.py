d4467a1176b8f2ee37ac01a271305168d0f1971f
michaelham1 <michaelhamdani0@gmail.com>
2026-05-19 01:18:22 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import logging
 import os
 import json
-from datetime import datetime
+from datetime import datetime import pytz
 from dotenv import load_dotenv
 import gspread
 from google.oauth2.service_account import Credentials
@@ -106,7 +106,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
 
     text      = msg.text
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    wib = pytz.timezone("Asia/Jakarta") timestamp = datetime.now(wib).strftime("%Y-%m-%d %H:%M:%S")
 
     if ":" in text:
         data = parse_message(text)
